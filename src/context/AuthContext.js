a402e2bcc73b9d8be911148d9588adac9bf2d64c
Knifucrab <bringasmaurosebastian@gmail.com>
2024-10-31 16:57:35 -0300

    local login with token async storage verification and loading screen when app opens

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,4 +1,4 @@
-import React, {createContext, useState, useContext} from "react";
+import React, {createContext, useState, useContext, useEffect} from "react";
 import outfyApi from "../api/outfyApi";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import {setUser, clearUser} from "../actions/userActions";
@@ -7,6 +7,7 @@ const AuthContext = createContext();
 
 export const AuthProvider = ({children}) => {
   const [user, setUserState] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   const login = async (email, password) => {
     // Here we request to the api login route to authenticate the user with his email and password and save the token that the api sends to us if the auth data is Ok
@@ -14,11 +15,12 @@ export const AuthProvider = ({children}) => {
       //Step 1: Auth and get token
 
       const response = await outfyApi.post("/auth/login", {email, password});
-      console.log(response);
+
       const {token} = response.data;
+      console.log(token);
       await AsyncStorage.setItem("token", token);
 
-      // setUserState(user);
+      setUserState({token});
       // dispatch(setUser(user));
       return true;
     } catch (error) {
@@ -34,10 +36,26 @@ export const AuthProvider = ({children}) => {
 
   const logout = async () => {
     await AsyncStorage.removeItem("token");
+    setUserState(null);
   };
 
+  const tryLocalLogin = async () => {
+    setLoading(true);
+    console.log("Trying local signin");
+    const token = await AsyncStorage.getItem("token");
+    if (token) {
+      console.log(token);
+      setUserState({token});
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    tryLocalLogin();
+  }, []);
+
   return (
-    <AuthContext.Provider value={{user, login, register, logout}}>
+    <AuthContext.Provider value={{user, login, register, logout, loading}}>
       {children}
     </AuthContext.Provider>
   );
--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -3,17 +3,32 @@ import {NavigationContainer} from "@react-navigation/native";
 import {createNativeStackNavigator} from "@react-navigation/native-stack";
 import LoginFlowNavigator from "./LoginFlowNavigator";
 import MainFlowNavigator from "./MainFlowNavigator";
+import {useAuth} from "../context/AuthContext";
+import {ActivityIndicator, View} from "react-native";
 
 const Stack = createNativeStackNavigator();
 
 const AppNavigator = ({customTheme}) => {
+  const {user, loading} = useAuth();
+
+  if (loading) {
+    return (
+      <View style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
+        <ActivityIndicator size="large" color={customTheme.colors.primary} />
+      </View>
+    );
+  }
+
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{headerShown: false}}>
-        <Stack.Screen name="LoginFlow" component={LoginFlowNavigator} />
-        <Stack.Screen name="MainFlow">
-          {() => <MainFlowNavigator customTheme={customTheme} />}
-        </Stack.Screen>
+        {user ? (
+          <Stack.Screen name="MainFlow">
+            {() => <MainFlowNavigator customTheme={customTheme} />}
+          </Stack.Screen>
+        ) : (
+          <Stack.Screen name="LoginFlow" component={LoginFlowNavigator} />
+        )}
       </Stack.Navigator>
     </NavigationContainer>
   );
